e077d777bda1e6eaf2c4d234fe9265eb7c2a70de
Søren Bendlin Gammelmark <gammelmark@gmail.com>
2022-12-12 22:25:16 +0100

    Update dysgame with bugfix for recursive functions

--- dysgame/dysgame-app-37c37fc8158e5f21.js
+++ dysgame/dysgame-app-37c37fc8158e5f21.js
@@ -596,12 +596,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper634 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 253, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper643 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 256, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1073 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 439, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper1082 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 442, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -639,7 +639,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('dysgame-app-1d143a4be1d491e9_bg.wasm', import.meta.url);
+        input = new URL('dysgame-app-37c37fc8158e5f21_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
